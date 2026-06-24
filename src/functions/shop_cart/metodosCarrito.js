1a505015618c637e22f58bdbba925d57abcef285
Daniel Zamora Paez <[email>
2026-06-24 15:35:54 -0500

    Tooltip para boton elimnar producto del carrito

--- src/functions/shop_cart/metodosCarrito.js
+++ src/functions/shop_cart/metodosCarrito.js
@@ -1,9 +1,8 @@
 import { producto } from "./claseProducto";
 
+// const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 document.addEventListener("DOMContentLoaded",
-    document.addEventListener("click",
-        function agregarCarrito(obj) {
-            
-        }
-    )//Cierre de escuchador de evento click cart buttom     
+    document.getElementById().innerHTML = ""
 );//Cierre de escuchador de evento carga de pagina web
